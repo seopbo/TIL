8947d9cc6921120741c7fce62de0f1fa32ef4fc9
aisolab <bsk0130@gmail.com>
2019-07-06 20:01:32 +0900

    binary search

--- Algorithm/binary_search.py
+++ Algorithm/binary_search.py
@@ -1,7 +1,20 @@
-from typing import List
+"""
+    implementing binary search
+"""
+from typing import List, Union
 
 
-def binary_search(list_of_items: List[int], item: int):
+def binary_search(list_of_items: List[int], item: int) -> Union[int, None]:
+    """Search index of target item in list of items
+
+    Args:
+        list_of_items : list of items
+        item: target item
+
+    Returns: index
+
+    """
+
     low = 0
     high = len(list_of_items) - 1
 
@@ -9,8 +22,14 @@ def binary_search(list_of_items: List[int], item: int):
         mid = (low + high) // 2
         if item == list_of_items[mid]:
             return mid
-        elif item > list_of_items[mid]:
+        if item > list_of_items[mid]:
             low = mid + 1
         else:
             high = mid - 1
     return None
+
+
+def test_binary_search():
+    """test binary_search"""
+    assert binary_search([1, 2, 3, 4, 5], 3) == 2
+    assert binary_search([1, 2, 3, 4, 5], 10) is None
