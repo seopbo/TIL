bd860569def8ca9f0567f14e4c3ddd13816d710a
aisolab <bsk0130@gmail.com>
2019-07-09 22:05:52 +0900

    Queue

---
+++ Data_structure/queue.py
@@ -0,0 +1,18 @@
+class Queue:
+    def __init__(self):
+        self._container = []
+
+    def enqueue(self, data):
+        self._container.append(data)
+
+    def dequeue(self):
+        return self._container.pop(0)
+
+    def empty(self):
+        if not self._container:
+            return True
+        else:
+            return False
+
+    def peek(self):
+        return self._container[0]
