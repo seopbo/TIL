ebbdf23a3f9a01bcc228cbdf49acf0a57dcead67
aisolab <bsk0130@gmail.com>
2019-10-05 11:16:50 +0900

    Sorting

--- Algorithm/Sorting/quick_sort.py
+++ Algorithm/Sorting/quick_sort.py
@@ -8,3 +8,12 @@ def quick_sort(seq):
     left = [x for i, x in enumerate(seq) if x <= pivot and i != ipivot]
     right = [x for i, x in enumerate(seq) if x > pivot and i != ipivot]
     return quick_sort(left) + [pivot] + quick_sort(right)
+
+
+def test_quick_sort():
+    seq = [3, 5, 2, 6, 8, 1, 0, 3, 5, 6, 2]
+    assert quick_sort(seq) == sorted(seq)
+
+
+if __name__ == "__main__":
+    test_quick_sort()
